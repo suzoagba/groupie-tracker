ec52ff2be8fc34a957c8260b74e9426df4f084ad
suzoagba <samuzoagba@gmail.coma>
2023-04-01 15:49:54 +0300

    1

--- handler/handler.go
+++ handler/handler.go
@@ -10,24 +10,30 @@ import (
 	"regexp"
 	"strconv"
 )
-
+// Handler function for HTTP requests
 func Handler(w http.ResponseWriter, r *http.Request) {
+	// Recover from any panics that may occur during request handling
 	defer func() {
 		if err := recover(); err != nil {
 			log.Println("PANIC:", err)
 			errorHandler(w, http.StatusInternalServerError)
 		}
 	}()
+	// Validate request header
 	if !validateRequest(r.Header) {
 		errorHandler(w, http.StatusBadRequest)
 		return
 	}
+	// Handle requests based on URL path
 	switch r.URL.Path {
 	case "/":
+		// Parse template files
 		tmpl, err := template.ParseFiles("templates/index.gohtml", "templates/filters.gohtml", "templates/artists.gohtml", "templates/artistInfo.gohtml")
 		if err != nil {
+			// Panic if there is an error parsing the template files
 			panic(err)
 		}
+		// Filter artists based on query parameters
 		artists.FilterArtists(r.FormValue("search"), r.FormValue("creationDateFrom"), r.FormValue("creationDateTo"), r.FormValue("firstAlbumDateFrom"), r.FormValue("firstAlbumDateTo"), r.Form["numMembers"], r.FormValue("locations"))
 		formValues(r.FormValue("search"), r.FormValue("locations"), r.Form["numMembers"], r.FormValue("firstAlbumDateFrom"), r.FormValue("firstAlbumDateTo"), r.FormValue("creationDateTo"), r.FormValue("creationDateFrom"))
 		if err := tmpl.Execute(w, variables.NewPage); err != nil {
@@ -37,9 +43,10 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		errorHandler(w, http.StatusNotFound)
 	}
 }
-
+// errorHandler function for generating error messages
 func errorHandler(w http.ResponseWriter, status int) {
 	w.WriteHeader(status)
+	// Generate error message based on status code
 	errorStruct := make(map[string]string)
 	errorStruct["ErrorNumber"] = strconv.Itoa(status)
 	tmpl, err := template.ParseFiles("templates/error.gohtml")
